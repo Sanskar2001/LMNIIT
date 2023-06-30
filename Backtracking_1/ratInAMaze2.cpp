8f39d42e116d098c1175773f61be68cb43ab9771
Sanskar2001 <sanskaratrey22@gmail.com>
2023-06-30 23:40:19 +0530

    lec3

--- Backtracking_1/ratInAMaze2.cpp
+++ Backtracking_1/ratInAMaze2.cpp
@@ -26,12 +26,35 @@ void cntPaths(int maze[][4],int x,int y,int n)
 
 }
 
+// int cntPaths2(int maze[][4],int x,int y,int n)
+// {
+
+// 	if(x>=n || y>=n)
+// 		return 0;
+
+// 	if(maze[x][y]==0)
+// 		return 0;
+
+// 	if(x==n-1 && y==n-1)
+// 	{
+// 		return 1;
+// 	}
+	
+
+// 	int left=cntPaths2(maze,x,y+1,n); // cnt the paths in rt dirn
+// 	int right=cntPaths2(maze,x+1,y,n); // cnt the paths in down dirn
+
+
+// 	return left+right;
+
+// }
+
 
 int main()
 {
 
 	int board[][4]={{1,1,1,1},
-					{0,1,0,1},
+					{1,1,0,1},
 					{0,1,0,1},
 					{0,1,1,1},
 					};
@@ -39,6 +62,7 @@ int main()
 	int n=4;
 
 	cntPaths(board,0,0,n);
-
 	cout<<"Total ways="<<cnt;
+
+	// cout<<cntPaths2(board,0,0,n)<<endl;
 }
