5fce07dc1ecf53c26c4064e5862d9685e4acc2bc
Sanskar2001 <sanskaratrey22@gmail.com>
2023-06-30 23:34:48 +0530

    lec3

---
+++ Backtracking_1/ratInAMaze2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+// Return type??
+// void
+
+int cnt=0;
+
+void cntPaths(int maze[][4],int x,int y,int n)
+{
+
+	if(x>=n || y>=n)
+		return;
+
+	if(maze[x][y]==0)
+		return;
+
+	if(x==n-1 && y==n-1)
+	{	cnt++;
+		return;
+	}
+
+	cntPaths(maze,x,y+1,n); // cnt the paths in rt dirn
+	cntPaths(maze,x+1,y,n); // cnt the paths in down dirn
+
+}
+
+
+int main()
+{
+
+	int board[][4]={{1,1,1,1},
+					{0,1,0,1},
+					{0,1,0,1},
+					{0,1,1,1},
+					};
+
+	int n=4;
+
+	cntPaths(board,0,0,n);
+
+	cout<<"Total ways="<<cnt;
+}
